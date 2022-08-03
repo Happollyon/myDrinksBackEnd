7192d7a2d7b300c3e89cbdf87f21967927dba248
Fagner Nunes <fagnernunes11@gmail.com>
2022-08-03 23:07:36 +0100

    added sql verssion

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-import imp
+
 from flask import Flask, jsonify
-from sqlalchemy import create_engine, false
+from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session,sessionmaker
 import os
 
 
 app = Flask(__name__)
-app.config.update(dict(
-    DEBUG = True
-))
+
 engine = create_engine('postgresql://dmcjgfxxlzkwzz:54d357220eb56acc5bc27a98ace88fdc63a94a6837378d415896997085118fcf@ec2-52-19-188-149.eu-west-1.compute.amazonaws.com:5432/d48equ8fqv99ao')
 
 db = scoped_session(sessionmaker(bind=engine))
